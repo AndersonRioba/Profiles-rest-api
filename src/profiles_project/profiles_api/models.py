a10b854e47462e7d95a59645ccb41d8144858441
AndersonRioba <anderson.rioba@gmail.com>
2019-03-12 15:10:49 +0300

    Reduced the import statements to only one line since they all use django contrib auth models

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin
-from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
 
